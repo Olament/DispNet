c328d613851c81aa195d51360af0fb99458a8786
Zixuan Guo <i@zxguo.me>
2020-04-23 11:44:20 -0500

    add random flip and crop

--- dataloader.py
+++ dataloader.py
@@ -1,9 +1,10 @@
 import torch
 import os
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy as np
 import cv2
 import torchvision.transforms as transforms
+import random
 
 class KITTIDataset(torch.utils.data.Dataset):
     def __init__(self, mode='train'):
@@ -36,13 +37,13 @@ class KITTIDataset(torch.utils.data.Dataset):
                                                    contrast=(0.8, 1.2), 
                                                    saturation=(0.8, 1.2), 
                                                    hue=(-0.1, 0.1)),
-                            transforms.ToTensor()
             ])
         else:
             image_trans = transforms.Compose([
                             transforms.Resize((375, 1424)),
-                            transforms.ToTensor()
             ])
+
+        totensor_trans = transforms.ToTensor()
 
         image = Image.open(self.img_lst[index])
         image = image_trans(image)
@@ -51,13 +52,33 @@ class KITTIDataset(torch.utils.data.Dataset):
         depth = cv2.resize(depth, dsize=(1424, 375), interpolation=cv2.INTER_NEAREST)
         depth = depth.astype(float)
         depth /= 256.0
-        
-        #if self.mode == 'train':
-        #    mask = (depth == 0.0).astype(float) # mask zero
-        #    depth += mask
-        #    depth = np.reciprocal(depth)
-        #    depth -= mask
 
-        depth = torch.from_numpy(depth) 
+        if self.mode == 'train':
+            depth = Image.fromarray(depth) # convert numpy to PIL image
+            # random horizontal flip
+            if random.random() > 0.5:
+                depth = ImageOps.mirror(depth)
+                image = ImageOps.mirror(image)
+
+            image = totensor_trans(depth)
+            depth = totensor_trans(depth)
+
+            image, depth = random_crop(image, depth, 1324, 275)
+        else:
+            image = totensor_trans(depth)
+            depth = torch.from_numpy(depth)
 
         return image, depth
+
+
+def random_crop(image, depth, height, width):
+    assert image.shape[1] >= height
+    assert image.shape[2] >= width
+    assert image.shape[1] == depth.shape[1]
+    assert image.shape[2] == depth.shape[2]
+
+    x = random.randint(0, image.shape[2] - width)
+    y = random.randint(0, image.shape[1] - height)
+    image = image[:, y:y+height, x:x + width]
+    depth = depth[:, y:y+height, x:x + width]
+    return image, depth
